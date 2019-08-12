15f3f0f2864574eef00835727cd5ae9502b879b7
Erik Olofsson <erik@olofsson.info>
2019-08-12 21:51:00 +0200

    [Malterlib/Mongo] Update import cache

--- ImportCache/External_mongo-c-driver-Linux2.6-x86-Release/src/libmongoc/src/mongoc/mongoc-config.h
+++ ImportCache/External_mongo-c-driver-Linux2.6-x86-Release/src/libmongoc/src/mongoc/mongoc-config.h
@@ -22,10 +22,10 @@
  * used to compile mongoc */
 #define MONGOC_USER_SET_CFLAGS ""
 
-#define MONGOC_USER_SET_LDFLAGS "-Xlinker -rpath-link -Xlinker "/CompiledFiles/Dependencies/Binaries/Malterlib_Linux/SDKs/Linux2.6.sdk/lib32" -Xlinker -rpath-link -Xlinker "/CompiledFiles/Dependencies/Binaries/Malterlib_Linux/SDKs/Linux2.6.sdk/usr/lib32""
+#define MONGOC_USER_SET_LDFLAGS "-Xlinker -rpath-link -Xlinker "/CompiledFiles/Dependencies/Binaries/Malterlib_Linux/SDKs/Linux2.6.sdk/lib32" -Xlinker -rpath-link -Xlinker "/CompiledFiles/Dependencies/Binaries/Malterlib_Linux/SDKs/Linux2.6.sdk/usr/lib32" -fuse-ld=lld -stdlib=libc++"
 
 /* MONGOC_CC is used to determine what C compiler was used to compile mongoc */
-#define MONGOC_CC "/CompiledFiles/Dependencies/llvm/8.0/build/main/bin/clang"
+#define MONGOC_CC "/CompiledFiles/Dependencies/llvm/9.0/build/main/bin/clang"
 
 /*
  * MONGOC_ENABLE_SSL_SECURE_CHANNEL is set from configure to determine if we are
