39df3fb1689232f2f3308f4dcc3107b6f43e6c5c
Erik Olofsson <erik@olofsson.info>
2023-08-27 19:44:53 +0200

    [Malterlib/Mongo] Update import cache

--- ImportCache/External_mongo-c-driver-Windows-x86-Debug/DartConfiguration.tcl
+++ ImportCache/External_mongo-c-driver-Windows-x86-Debug/DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: .
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: erikwindev02
+Site: ERIKOLOFSSO8224
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Win32-ninja
@@ -63,7 +63,7 @@ UpdateOptions:
 UpdateType: git
 
 # Compiler info
-Compiler: C:/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.37.32822/bin/Hostx64/x86/cl.exe
+Compiler: C:/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.37.32822/bin/Hostarm64/x86/cl.exe
 CompilerVersion: 19.37.32822.0
 
 # Dynamic analysis (MemCheck)
@@ -81,7 +81,7 @@ MemoryCheckCommandOptions:
 MemoryCheckSuppressionFile: 
 
 # Coverage
-CoverageCommand: C:/Strawberry/c/bin/gcov.exe
+CoverageCommand: COVERAGE_COMMAND-NOTFOUND
 CoverageExtraFlags: -l
 
 # Testing options
--- ImportCache/External_mongo-c-driver-macOS-x64-Debug/DartConfiguration.tcl
+++ ImportCache/External_mongo-c-driver-macOS-x64-Debug/DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: .
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: erikmbp09
+Site: eolofsson0523
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Darwin-clang++
