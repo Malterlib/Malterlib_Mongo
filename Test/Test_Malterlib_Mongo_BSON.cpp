5a466db9265a8cb922cf7b162867173a7a873215
Erik Olofsson <erik@olofsson.info>
2023-10-08 17:54:26 +0200

    [Malterlib/Mongo] Switch to llvm 17

--- Test/Test_Malterlib_Mongo_BSON.cpp
+++ Test/Test_Malterlib_Mongo_BSON.cpp
@@ -112,21 +112,21 @@ namespace
 						, "k_int32"_o= CEJSONUserTypeOrdered{"int32", int32(556)}
 						, "k_int64"_o= int64(constant_int64(66554466556665))
 						, "k_binary"_o= TestBinary
-						, "k_binary k_function"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "Function"}}
-						, "k_binary k_binary_deprecated"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "ByteArrayDeprecated"}}
-						, "k_binary k_uuid_deprecated"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "bdtUUID"}}
-						, "k_binary k_uuid"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "newUUID"}}
-						, "k_binary k_md5"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "MD5Type"}}
-						, "k_binary k_user"_o= CEJSONUserTypeOrdered{"BinData", {"Data"__o= fg_Base64Encode(TestBinary), "Type"__o= "bdtCustom"}}
+						, "k_binary k_function"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "Function"}}
+						, "k_binary k_binary_deprecated"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "ByteArrayDeprecated"}}
+						, "k_binary k_uuid_deprecated"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "bdtUUID"}}
+						, "k_binary k_uuid"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "newUUID"}}
+						, "k_binary k_md5"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "MD5Type"}}
+						, "k_binary k_user"_o= CEJSONUserTypeOrdered{"BinData", {"Data"_jo= fg_Base64Encode(TestBinary), "Type"_jo= "bdtCustom"}}
 						, "k_undefined"_o= CEJSONUserTypeOrdered{"Undefined", 1}
 						, "k_oid"_o= CEJSONUserTypeOrdered{"jstOID", TestOID.c_str()}
-						, "k_regex"_o= CEJSONUserTypeOrdered{"RegEx", {"Regex"__o= "RegEx", "RegexFlags"__o= "ls"}}
-						, "k_dbpointer"_o= CEJSONUserTypeOrdered{"DBRef", {"NS"__o= "Collection", "ObjectID"__o= TestOID.c_str()}}
+						, "k_regex"_o= CEJSONUserTypeOrdered{"RegEx", {"Regex"_jo= "RegEx", "RegexFlags"_jo= "ls"}}
+						, "k_dbpointer"_o= CEJSONUserTypeOrdered{"DBRef", {"NS"_jo= "Collection", "ObjectID"_jo= TestOID.c_str()}}
 						, "k_code"_o= CEJSONUserTypeOrdered{"Code", "TestCode"}
 						, "k_symbol"_o= CEJSONUserTypeOrdered{"Symbol", "TestSymbol"}
-						, "k_codewscope"_o= CEJSONUserTypeOrdered{"CodeWScope", {"Code"__o= "TestCode", "Scope"__o= {"Test1"__o= "Test2"}}}
-						, "k_timestamp"_o= CEJSONUserTypeOrdered{"Timestamp", {"Seconds"__o= 555, "Increment"__o= 666}}
-						, "k_decimal128"_o= CEJSONUserTypeOrdered{"Decimal128", {"High"__o= constant_uint64(55555555555), "Low"__o= constant_uint64(66666666666)}}
+						, "k_codewscope"_o= CEJSONUserTypeOrdered{"CodeWScope", {"Code"_jo= "TestCode", "Scope"_jo= {"Test1"_jo= "Test2"}}}
+						, "k_timestamp"_o= CEJSONUserTypeOrdered{"Timestamp", {"Seconds"_jo= 555, "Increment"_jo= 666}}
+						, "k_decimal128"_o= CEJSONUserTypeOrdered{"Decimal128", {"High"_jo= constant_uint64(55555555555), "Low"_jo= constant_uint64(66666666666)}}
 						, "k_minkey"_o= CEJSONUserTypeOrdered{"MinKey", 1}
 						, "k_maxkey"_o= CEJSONUserTypeOrdered{"MaxKey", 1}
 					}
