674886de20979fc99347f9c4ef55359c898b0dbd
Erik Olofsson <erik.olofsson@favro.com>
2024-02-19 14:19:21 +0100

    [Malterlib/Mongo] Update import cache

--- ImportCache/External_mongo-c-driver-Linux-x86-Release/src/libmongoc/src/mongoc/mongoc-config.h
+++ ImportCache/External_mongo-c-driver-Linux-x86-Release/src/libmongoc/src/mongoc/mongoc-config.h
@@ -38,7 +38,7 @@
  * used to compile mongoc */
 #define MONGOC_USER_SET_CFLAGS ""
 
-#define MONGOC_USER_SET_LDFLAGS "-fuse-ld=lld -stdlib=libc++ -Wno-unused-command-line-argument -L"../../../../../../../../Binaries/MalterlibLLVM/Linux/x86/lib/i386-unknown-linux-gnu" -Xlinker -rpath-link -Xlinker ../../../../../../../../Binaries/MalterlibSDK/Linux/x86/Linux.sdk/lib32 -Xlinker -rpath-link -Xlinker ../../../../../../../../Binaries/MalterlibSDK/Linux/x86/Linux.sdk/usr/lib32"
+#define MONGOC_USER_SET_LDFLAGS "-fuse-ld=lld -stdlib=libc++ -Wno-unused-command-line-argument -L"../../../../../../../../Binaries/MalterlibLLVM/Linux/x86/lib/i686-unknown-linux-gnu" -Xlinker -rpath-link -Xlinker ../../../../../../../../Binaries/MalterlibSDK/Linux/x86/Linux.sdk/lib32 -Xlinker -rpath-link -Xlinker ../../../../../../../../Binaries/MalterlibSDK/Linux/x86/Linux.sdk/usr/lib32"
 
 /* MONGOC_CC is used to determine what C compiler was used to compile mongoc */
 #define MONGOC_CC "../../../../../../../../Binaries/MalterlibLLVM/macOS/arm64/bin/clang"
